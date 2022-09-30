c6ec636f6a44388012792bc379151dd95915811e
RogerioJunor <rogeriopcjr@gmail.com>
2022-09-30 12:16:23 -0300

    create page login and register

---
+++ frontend/src/App.jsx
@@ -0,0 +1,98 @@
+import "./App.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+
+function App() {
+  const handleClickRegister = (values) => { return console.log(values); };
+  const handleClickLogin = (values) => {
+    return console.log(values);
+  };
+
+  const validationLogin = yup.object().shape({
+    email: yup.string().email("email e/ou senha incorretos").required("Campo obrigatório"),
+    password: yup.string().min(6, "email e/ou senha incorretos").required("Campo obrigatório"),
+  });
+
+  const validationRegister = yup.object().shape({
+    email: yup.string().email("email e/ou senha incorretos").required("Campo obrigatório"),
+    password: yup.string().min(6, "A senha deve ter no minimo 6 caracteres").required("Campo obrigatório"),
+    confirmPassword : yup.string().oneOf([yup.ref("password"), null], "As senhas não são iguais"),
+  });
+
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <Formik
+        initialValues={{}}
+        onSubmit={handleClickLogin}
+        validationSchema={validationLogin}
+      >
+        <Form className="login-form">
+          <div className="login-form-group">
+            <Field name="email" className="form-field" placeholder="Email" />
+            <ErrorMessage
+              component="span"
+              name="email"
+              className="form-error"
+            />
+          </div>
+          <div className="login-form-group">
+            <Field name="password" className="form-field" placeholder="Senha" />
+            <ErrorMessage
+              component="span"
+              name="password"
+              className="form-error"
+            />
+          </div>
+
+          <button className="button" type="submit">
+            Login
+          </button>
+        </Form>
+      </Formik>
+
+
+
+      <h1>Cadastro</h1>
+      <Formik
+        initialValues={{}}
+        onSubmit={handleClickRegister}
+        validationSchema={validationRegister}
+      >
+        <Form className="register-form">
+          <div className="register-form-group">
+            <Field name="email" className="form-field" placeholder="Email" />
+            <ErrorMessage
+              component="span"
+              name="email"
+              className="form-error"
+            />
+          </div>
+          <div className="register-form-group">
+            <Field name="password" className="form-field" placeholder="Senha" />
+            <ErrorMessage
+              component="span"
+              name="password"
+              className="form-error"
+            />
+          </div>
+
+          <div className="register-form-group">
+            <Field name="confirmPassword" className="form-field" placeholder="Confirme sua senha" />
+            <ErrorMessage
+              component="span"
+              name="confirmPassword"
+              className="form-error"
+            />
+          </div>
+
+          <button className="button" type="submit">
+            Login
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default App;
