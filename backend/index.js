46da8fcfc891af2be5aaf0aa1aa9ed025874fea3
RogerioJunor <rogeriopcjr@gmail.com>
2022-09-30 14:18:53 -0300

    configure mysql

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
 
-app.get('/', (req, res) => {
-  res.send('GET request to the homepage')
-})
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "bd_page_login"
+});
+
+/*app.get('/', (req, res) => {
+    db.query("INSERT INTO users (email, password) VALUES ('teste@gmail.com', '123456')", (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+    });
+});*/
 
 app.listen(3001, () => {
     console.log("Rodando na porta 3001");
